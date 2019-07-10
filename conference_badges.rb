278b6d36592f1410923c229191c7c21625bd249b
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 20:57:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,12 @@
 # Write your code here.
-def batch_badge_creator
+def batch_badge_creator(array)
+  array.each do |speaker|
+    badge_maker(speaker)
+  end
 end
 
 def badge_maker(name)
   puts "Hello, my name is" + name "."
 end
+
+assign_rooms
